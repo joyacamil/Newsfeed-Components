5eef58849cf61b8936a8192c325b40af5ddcc797
Deshauna Rainer <deshaunarainer@gmail.com>
2019-09-12 22:20:42 -0400

    finished step one

--- Article/Article.js
+++ Article/Article.js
@@ -102,8 +102,39 @@ const data = [
   Hint: You will need to use createElement more than once here!
 
   Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
+const articles = document.querySelector('.articles')
+*/
 
-  Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
+function Article(title, date, firstParagraph, secondParagraph, thirdParagraph) {
+
+  const article = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const p3 = document.createElement('p');
+  const span = document.createElement('span');
+
+  article.classList.add('article');
+  p.classList.add('date');
+  span.classList.add('expandButton');
+
+  article.appendChild(h2);
+  article.appendChild(p);
+  article.appendChild(p1);
+  article.appendChild(p2);
+  article.appendChild(p3);
+  article.appendChild(span);
+
+  h2.textContent = 'title';
+  p.textContent = 'date';
+  p1.textContent = 'firstParagraph';
+  p2.textContent = 'secondParagraph';
+  p3.textContent = 'thirdParagraph';
+  span.textContent = 'expand';
+}
+
+/*  Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
 
   Step 3: return the entire component.
 
